572660e72851a76fe31a246e296f7fa63f523394
angryCar80 <aymenouabbas@gmail.com>
2025-07-03 11:07:04 +0100

    Feat: Made The Structur

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <vector>
 #include "raylib.h"
 
+struct Body{
+  Vector2 pos;
+  float vel;
+  float mass;
+  float tint;
+};
+
 int main (int argc, char *argv[]) {
-  
+  int width = 800;
+  int height = 800;
+  InitWindow(width, height, "Gravity Simulation");
+  SetTargetFPS(60);
+
+  std::vector<Body> bodies = {
+
+  };
+
+  while(!WindowShouldClose()){
+    BeginDrawing();
+
+    EndDrawing();
+  }
   return 0;
 }
