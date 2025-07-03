dc7127870cb61f6bfe79de5c9924509b504bfc3a
angryCar80 <aymenouabbas@gmail.com>
2025-07-03 18:17:28 +0100

    Feat: Draw The Planets

--- main.cpp
+++ main.cpp
@@ -1,26 +1,32 @@
+#include <cmath>
 #include <vector>
+
 #include "raylib.h"
 
-struct Body{
+struct Body {
   Vector2 pos;
   Vector2 vel;
   float mass;
   Color tint;
 };
 
-int main (int argc, char *argv[]) {
-  int width = 800;
-  int height = 800;
+int main(int argc, char *argv[]) {
+  int width = 400;
+  int height = 400;
   InitWindow(width, height, "Gravity Simulation");
   SetTargetFPS(60);
 
   std::vector<Body> bodies = {
-    {{100, 100}, {0, 0}, 50000, GREEN },
+      {{100, 100}, {0, 0}, 10000, GREEN},
+      {{300, 300}, {0, 0}, 20000, LIGHTGRAY},
+      {{200, 200}, {0, 0}, 50000, YELLOW},
   };
 
-
-  while(!WindowShouldClose()){
+  while (!WindowShouldClose()) {
     BeginDrawing();
+    for (auto &b : bodies) {
+      DrawCircleV(b.pos, 4 + logf(b.mass), b.tint);
+    }
 
     EndDrawing();
   }
